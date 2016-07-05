0d57e427c99c7a83584ff4cbaf29986827597a21
smct <1315917709@qq.com>
2016-07-05 23:53:19 +0800

    建立inputActivity

---
+++ app/src/main/java/com/mtwei/ui/InputActivity.java
@@ -0,0 +1,17 @@
+package com.mtwei.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mtwei.R;
+
+/**
+ * Created by Administrator on 2016/7/5 0005.
+ */
+public class InputActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
